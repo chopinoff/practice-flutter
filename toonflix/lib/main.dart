a6cbd2b80acd26890b4227da57f3807fa94a1f50
HY KIM <chopinoff@gmail.com>
2023-06-26 10:32:52 +0900

    230626_Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,39 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // 컬러 입력 형식
-        // 1. Colors.black.withOpacity(1)
-        // 2. Color(0xFF000000)
-        // 3. Color.fromARGB(1, 0, 0, 0)
-        backgroundColor: Color(
+        backgroundColor: const Color(
           0xFF181818,
         ),
         body: Padding(
-          padding: EdgeInsets.symmetric(
+          padding: const EdgeInsets.symmetric(
             horizontal: 40,
           ),
           child: Column(
+            // Column으로 쌓이는 Widget들이 앞으로 왼쪽 정렬로 쌓이게 됨
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 80,
               ),
               Row(
-                // Row의 mainAxisAlignment는 Row 방향, crossAxisAlignment는 Column 방향
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
-                    // Column의 mainAxisAlignment는 Column 방향, crossAxisAlignment는 Row 방향
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text(
+                      const Text(
                         "Hey, Horongee",
                         style: TextStyle(
                           color: Colors.white,
@@ -44,13 +42,66 @@ class App extends StatelessWidget {
                       Text(
                         "Welcome back!",
                         style: TextStyle(
-                          // 색상 미리보기 : Colors. 자동완성된 후 Ctrl + SpaceBar
                           color: Colors.white.withOpacity(0.8),
                           fontSize: 18,
                         ),
                       ),
                     ],
                   )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                // 변수를 위한 $가 아니라는 것을 표시하기 위해 \$로 작성
+                "\$5 194 482",
+                style: TextStyle(
+                  fontSize: 38,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  // Container : HTML의 div와 유사한 Widget
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      // 1. 모서리 중 일부만 적용하고 싶을 때
+                      // borderRadius: BorderRadius.vertical(
+                      //   top: Radius.circular(20),
+                      //   bottom: Radius.circular(40),
+                      // ),
+                      // 2. 모서리 전체 적용할 때 : all 생략 가능
+                      borderRadius: BorderRadius.circular(55),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 40,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ],
