93398b49199343a4dc8832e5f8c7ca6fdadeea84
HY KIM <chopinoff@gmail.com>
2023-06-27 17:03:05 +0900

    230627_StatusBar 색상 변경

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:toonflix/screens/pomodoro_app/pomodoro_home_screen.dart';
 import 'package:toonflix/themes/pomodoro_theme.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Color.fromRGBO(0, 0, 0, 0)));
   runApp(const App());
 }
 
